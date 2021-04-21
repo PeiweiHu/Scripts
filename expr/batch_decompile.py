c2721fb1cc9fc69133207515e1634b8551f04817
PeiweiHu <jlu.hpw@foxmail.com>
2021-04-21 11:16:32 +0800

    delete something

--- expr/batch_decompile.py
+++
@@ -1,3 +0,0 @@
-'.\support\analyzeHeadless.bat . ghidra-test -import C:\Users\xxx\Desktop\a.exe -scriptPath . -postScript dec.py -deleteProject'
-'ida -Ohexrays:outfile.c:ALL -A <filename>'
-'ida -Ohexrays:outfile.c:main -A <filename>'
